8ee04cf3413dfccde3976fe2a017698c13c9b3ec
shk0625 <tjfgml1238@gmail.com>
2023-12-15 23:49:48 +0900

    🐛 board id로 글 조회 가능

--- board/urls.py
+++ board/urls.py
@@ -5,7 +5,7 @@ from board.views import BoardList, BoardDetail
 urlpatterns = [
     path('boards/', BoardList.as_view(), name='board-list'),
     path('boards/post/', BoardDetail.as_view(), name='board-create'),
-    path('boards/get/', BoardDetail.as_view(), name='board-p-list'),
+    path('boards/get/<int:pk>/', BoardDetail.as_view(), name='board-auth-list'),
     path('boards/put/', BoardDetail.as_view(), name='board-update'),
     path('boards/delete/', BoardDetail.as_view(), name='board-delete'),
 ]
