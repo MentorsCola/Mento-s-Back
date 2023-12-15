11ad1264ea9b9f3ba1e9264adacba55f731ccbcc
shk0625 <tjfgml1238@gmail.com>
2023-12-15 12:29:22 +0900

    🎨

--- user/urls.py
+++ user/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
 
-from user.views import UserListView, LoginAPIView, RegisterAPIView, Logout
+from user.views import UserListView, RegisterAPIView, AuthAPIView
 
 urlpatterns = [
     path('register/', RegisterAPIView.as_view(), name='user-register'),
-    path('users/', UserListView.as_view(), name='user-list, user-signup'),
-    path('login/', LoginAPIView.as_view(), name='user-login'),
-    path('logout/', Logout.as_view(), name='user-logout'),
+    path('users/', UserListView.as_view(), name='user-list'), #allowAnyone
+    path('users/', AuthAPIView.as_view(), name='user-list, user-login, logout'),
     path('auth/refresh/', TokenRefreshView.as_view()), # jwt 토큰 재발급
 ]
