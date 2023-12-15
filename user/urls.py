da8c8b58c72013fc9ac4f5f14142a145412c025c
shk0625 <tjfgml1238@gmail.com>
2023-12-15 14:00:56 +0900

    🎨 로그인까지완료

--- user/urls.py
+++ user/urls.py
@@ -5,7 +5,7 @@ from user.views import UserListView, RegisterAPIView, AuthAPIView
 
 urlpatterns = [
     path('register/', RegisterAPIView.as_view(), name='user-register'),
-    path('users/', UserListView.as_view(), name='user-list'), #allowAnyone
     path('users/', AuthAPIView.as_view(), name='user-list, user-login, logout'),
+    path('users/', UserListView.as_view(), name='user-list'), #allowAnyone
     path('auth/refresh/', TokenRefreshView.as_view()), # jwt 토큰 재발급
 ]
