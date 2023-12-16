475e20d8acf365ab45b49ad348c749669cd44c50
shk0625 <tjfgml1238@gmail.com>
2023-12-16 22:46:31 +0900

    ✨ comment model

---
+++ comment/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+from board.models import Board
+
+
+class Comment(models.Model):
+    id = models.AutoField(primary_key=True)
+    board = models.ForeignKey(Board, on_delete=models.CASCADE, related_name='comments')
+    content = models.TextField("댓글 내용", null=False)
+    nickname_commend = models.ForeignKey('Nicknames', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Comment #{self.id} for Board #{self.board_id}"
