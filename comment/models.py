97b20365f11661b83c78116ed7fdb4d9a774b691
shk0625 <tjfgml1238@gmail.com>
2023-12-16 23:09:37 +0900

    ♻️ null 가능

--- comment/models.py
+++ comment/models.py
@@ -7,7 +7,7 @@ from nicknames.models import Nicknames
 class Comment(models.Model):
     id = models.AutoField(primary_key=True)
     board = models.ForeignKey(Board, on_delete=models.CASCADE, related_name='comments')
-    content = models.TextField("댓글 내용", null=False)
+    content = models.TextField("댓글 내용", null=True)
     nickname_commend = models.ForeignKey(Nicknames, on_delete=models.CASCADE)
 
     def __str__(self):
