276ca3ea5e33076fe8ce6cf3eafd3deba582de6a
shk0625 <tjfgml1238@gmail.com>
2023-12-16 23:15:35 +0900

    ✨ 댓글 속성 추가

--- comment/models.py
+++ comment/models.py
@@ -6,8 +6,8 @@ from nicknames.models import Nicknames
 
 class Comment(models.Model):
     id = models.AutoField(primary_key=True)
-    board = models.ForeignKey(Board, on_delete=models.CASCADE, related_name='comments')
-    content = models.TextField("댓글 내용", null=True)
+    board = models.ForeignKey(Board, on_delete=models.CASCADE, related_name='comments',  null=True)
+    content = models.TextField("댓글 내용", null=False)
     nickname_commend = models.ForeignKey(Nicknames, on_delete=models.CASCADE)
 
     def __str__(self):
