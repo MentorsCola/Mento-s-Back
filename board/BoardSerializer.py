594b4772d6167d2b3437a5aded4383f90214a445
shk0625 <tjfgml1238@gmail.com>
2023-12-16 23:15:27 +0900

    ✨ 주석 추가 및 댓글 관련 필드 추가

--- board/BoardSerializer.py
+++ board/BoardSerializer.py
@@ -6,7 +6,7 @@ from board.models import Board
 from user.models import User
 
 
-class BoardSerializer(serializers.ModelSerializer):
+class BoardSerializer(serializers.ModelSerializer): # board post
     class Meta:
         model = Board
         fields = '__all__'
@@ -25,15 +25,15 @@ class BoardSerializer(serializers.ModelSerializer):
         return super().create(validated_data)
 
 
-class BoardNotLoginSerializer(serializers.ModelSerializer):
+class BoardNotLoginSerializer(serializers.ModelSerializer): #board get
     class Meta:
         model = Board
         fields = ['id', 'title', 'dt_created', 'dt_modified', 'nickname_author']
         read_only_fields = ['id', 'dt_created', 'dt_modified', 'nickname_author', 'author']
 
 
-class BoardLoginSerializer(serializers.ModelSerializer):
+class BoardLoginSerializer(serializers.ModelSerializer): #board get
     class Meta:
         model = Board
-        fields = ['id', 'title', 'content', 'dt_created', 'dt_modified', 'nickname_author']
-        read_only_fields = ['id', 'dt_created', 'dt_modified', 'nickname_author', 'author']
+        fields = ['id', 'title', 'content', 'dt_created', 'dt_modified', 'nickname_author', 'comments']
+        read_only_fields = ['id', 'dt_created', 'dt_modified', 'nickname_author', 'author' , 'comments']
