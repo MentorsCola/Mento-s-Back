e25d6c04a10ec555e5533f643e39b9a0deeabd31
shk0625 <tjfgml1238@gmail.com>
2023-12-18 08:56:46 +0900

    🎨

--- comment/models.py
+++ comment/models.py
@@ -1,11 +1,10 @@
 from django.db import models
-
 from nicknames.models import Nicknames
 
 
 class Comment(models.Model):
     id = models.AutoField(primary_key=True)
-    board = models.ForeignKey('board.Board', on_delete=models.CASCADE, related_name='comments',  null=True)
+    board = models.ForeignKey('board.Board', on_delete=models.CASCADE, related_name='comments', null=True)
     content = models.TextField("댓글 내용", null=False)
     nickname_commend = models.ForeignKey(Nicknames, on_delete=models.CASCADE)
 
