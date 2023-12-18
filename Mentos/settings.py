3b3619ebde2263f3172f41b5f17a12e92d312836
shk0625 <tjfgml1238@gmail.com>
2023-12-18 19:47:44 +0900

    cors adds

--- Mentos/settings.py
+++ Mentos/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
 
     # 생성한 앱
     'board',
@@ -104,6 +105,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'Mentos.urls'
