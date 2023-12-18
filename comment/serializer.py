4c1ccb9d3cf55e371cbd76a7dee19db30991f28c
shk0625 <tjfgml1238@gmail.com>
2023-12-18 23:39:01 +0900

    ♻️comment 댓글달기 성공~!

--- comment/serializer.py
+++ comment/serializer.py
@@ -5,4 +5,14 @@ from .models import Comment
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
-        fields = ['id', 'board', 'content', 'nickname']
+        fields = '__all__'
+        read_only_fields = ['nickname']
+
+    def create(self, validated_data):
+        # 현재 로그인한 사용자를 작성자로 설정
+        user = self.context['request'].user
+
+        # 사용자의 닉네임을 가져와서 할당
+        validated_data['nickname'] = user.id_nickname
+
+        return super().create(validated_data)
