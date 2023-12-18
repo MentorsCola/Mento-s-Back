d9751ed349f5833875d55f560f621a8c63107505
kkh061101 <kkh061101@naver.com>
2023-12-18 23:06:50 +0900

    중복 제거

--- Mentos/settings.py
+++ Mentos/settings.py
@@ -40,7 +40,7 @@ ALLOWED_HOSTS = []
 
 CORS_ALLOW_CREDENTIALS = True
 
-CORS_ORIGIN_WHITELIST = ['http://localhost:3000','http://127.0.0.1']
+CORS_ORIGIN_WHITELIST = ['http://localhost:3000','http://127.0.0.1:3000',]
 
 
 # Application definition
@@ -53,7 +53,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'corsheaders',
 
     # 생성한 앱
     'board',
@@ -99,7 +98,6 @@ SIMPLE_JWT = {
 }
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
