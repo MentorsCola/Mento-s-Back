50889bc10870c5ec70128e447a27a43f16096b49
shk0625 <tjfgml1238@gmail.com>
2023-12-19 00:34:37 +0900

    ♻️ 댓글 내용만 반환

--- comment/serializer.py
+++ comment/serializer.py
@@ -7,7 +7,7 @@ class CommentSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Comment
-        fields = ['id', 'content', 'nickname']
+        fields = ['content', 'nickname']
         read_only_fields = ['nickname', 'board']
 
     def create(self, validated_data):
