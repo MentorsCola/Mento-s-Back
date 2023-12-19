8799ab44b39fdc005d0b16d1968532bcc2888d1e
shk0625 <tjfgml1238@gmail.com>
2023-12-19 10:58:30 +0900

    ✨ tag models.py

---
+++ tag/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+from board.models import Board
+
+
+class Tag(models.Model):
+    id = models.AutoField(primery_key=True)
+    board = models.ForeignKey(Board, on_delete=models.CASCADE)
+    tag_content = models.TextField("tag", null=False)
+
+    def __str__(self):
+        return self.tag_content
