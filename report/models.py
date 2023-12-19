cbe86d83082530cb62423eeb4b0d1752c75286f0
shk0625 <tjfgml1238@gmail.com>
2023-12-19 23:28:03 +0900

    🐛  Field defines a relation with the model 'auth.User', which has been swapped out.         HINT: Update the relation to point at 'settings.AUTH_USER_MODEL'.

--- report/models.py
+++ report/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
+
+from Mentos import settings
 from board.models import Board
 
 
 class Report(models.Model):
-    reporter = models.ForeignKey(User, on_delete=models.CASCADE)
+    reporter = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     board = models.ForeignKey(Board, on_delete=models.CASCADE)
 
     def __str__(self):
