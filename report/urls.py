974fe31fbf869fccf752b490fc63ae85519cc1e0
shk0625 <tjfgml1238@gmail.com>
2023-12-20 00:06:35 +0900

    ♻️ report 경로 수정

--- report/urls.py
+++ report/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import ReportCreateView
 
 urlpatterns = [
-    path('report/<int:board_id>/', ReportCreateView.as_view(), name='report-create'),
+    path('reports/<int:board_id>/', ReportCreateView.as_view(), name='report-create'),
 ]
