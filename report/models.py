029a898a5888f872006e52f4da13b2fba3cf2974
shk0625 <tjfgml1238@gmail.com>
2023-12-19 23:14:57 +0900

    ✨신고 테이블

--- report/models.py
+++ report/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
+from board.models import Board
 
-# Create your models here.
+
+class Report(models.Model):
+    reporter = models.ForeignKey(User, on_delete=models.CASCADE)
+    board = models.ForeignKey(Board, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Report #{self.id} - {self.reporter.username} on {self.board.title}"
