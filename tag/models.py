9861ab250fab047c034d6edcbc356fb5be5bcbae
shk0625 <tjfgml1238@gmail.com>
2023-12-19 17:45:45 +0900

    board_id 추가해주기 null True

--- tag/models.py
+++ tag/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 
+from board.models import Board
+
 
 class Tag(models.Model):
     tag_content = models.CharField("Tag Content", max_length=255, unique=True)
+    board = models.ForeignKey(Board, on_delete=models.CASCADE, related_name='tags', null=True)
 
     def __str__(self):
         return self.tag_content
