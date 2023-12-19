63011c6d514cb95e77dd51f193b29e4b91ea66cd
shk0625 <tjfgml1238@gmail.com>
2023-12-20 00:06:42 +0900

    ♻️ report 시리얼라이저 유저 저장

--- report/serializer.py
+++ report/serializer.py
@@ -6,4 +6,10 @@ class ReportSerializer(serializers.ModelSerializer):
     class Meta:
         model = Report
         fields = ['board']
-        read_only_fields = ['reporter']
+        read_only_fields = ['reporter', 'board']
+
+    def create(self, validated_data):
+        # 현재 로그인한 사용자를 작성자로 설정
+        user = self.context['request'].user
+
+        return super().create(user)
