12ac4d9dd180dc59a34e853e9f389191cd015ad1
shk0625 <tjfgml1238@gmail.com>
2023-12-19 17:10:20 +0900

    ♻️

--- tag/urls.py
+++ tag/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from .views import TagCreateView
 
 urlpatterns = [
-    # 다른 URL 패턴들...
     path('tags/<int:board_id>/', TagCreateView.as_view(), name='tag-create'),
 ]
