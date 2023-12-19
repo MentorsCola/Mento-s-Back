1000c0e0166ffb799de2bccd3574df4c8221cf9c
shk0625 <tjfgml1238@gmail.com>
2023-12-19 15:25:04 +0900

    ✨tag model

--- tag/models.py
+++ tag/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Tag(models.Model):
+    tag_content = models.CharField("Tag Content", max_length=255, unique=True)
+
+    def __str__(self):
+        return self.tag_content
