84670b69207c00618a38593dce8c8311f24ae588
shk0625 <tjfgml1238@gmail.com>
2023-12-19 23:28:26 +0900

    ✨ report view

--- report/views.py
+++ report/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from report.serializer import ReportSerializer
 
-# Create your views here.
+
+class ReportCreateView(generics.CreateAPIView):
+    serializer_class = ReportSerializer
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, board_id):
+        # 신고 생성
+        serializer = ReportSerializer(data={'reporter': request.user.id, 'board': board_id, 'reason': request.data.get('reason')})
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
