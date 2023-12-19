30324bf6d2417ef7a8c149028c1519e7d03ab930
kkh061101 <kkh061101@naver.com>
2023-12-19 17:04:31 +0900

    board pk -> int pk

--- board/urls.py
+++ board/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('boards/', BoardList.as_view(), name='board-list'),
     path('boards/post/', BoardDetail.as_view(), name='board-create'),
     path('boards/get/<int:pk>/', BoardDetail.as_view(), name='board-auth-list'),
-    path('boards/put/', BoardDetail.as_view(), name='board-update'),
-    path('boards/delete/', BoardDetail.as_view(), name='board-delete'),
+    path('boards/put/<int:pk>/', BoardDetail.as_view(), name='board-update'),
+    path('boards/delete/<int:pk>/', BoardDetail.as_view(), name='board-delete'),
 ]
